bc0050d9165b9fbbcfb1b2f08811cd8989917e32
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2019-04-16 13:43:50 +1000

    Support webpack 4, check for warnings in child compilers

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 function ErrorOnWarningsPlugin() {};
 
-ErrorOnWarningsPlugin.prototype.apply = function(compiler) {
-  compiler.plugin('should-emit', function(compilation) {
+ErrorOnWarningsPlugin.prototype.apply = (compiler) => {
+  compiler.hooks.shouldEmit.tap('ErrorOnWarningsPlugin', (compilation) => {
+    let result;
     if (compilation.warnings && compilation.warnings.length) {
-      compilation.errors.push("one or more files: Warnings are not allowed.");
-      return false;
+      compilation.errors.push("Error: Warnings are not allowed.");
+      result = false;
     }
+
+    if (compilation.children) {
+      compilation.children.forEach((child) => {
+        if (child.warnings && child.warnings.length) {
+          compilation.errors.push(
+            `Error in child compiler${
+              child.name ? ` ${child.name}` : ' '
+            }: Warnings are not allowed.`
+          );
+          result = false;
+        }
+      });
+    }
+    return result;
   });
 };
 
