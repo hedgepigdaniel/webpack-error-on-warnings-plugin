840f20c66fe8b9a755cec632007da42e8df2ca9d
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2020-02-18 15:22:09 +1100

    convert warnings to errors instead of generating a new error

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,17 @@ import { Plugin } from 'webpack';
 export default class ErrorOnWarningsPlugin implements Plugin {
   apply(compiler) {
     compiler.hooks.shouldEmit.tap('ErrorOnWarningsPlugin', (compilation) => {
-      let result;
-      if (compilation.warnings && compilation.warnings.length) {
-        compilation.errors.push("Error: Warnings are not allowed.");
-        result = false;
+      if (compilation.warnings.length > 0) {
+        compilation.errors = compilation.errors.concat(compilation.warnings);
+        compilation.warnings = [];
       }
   
-      if (compilation.children) {
-        compilation.children.forEach((child) => {
-          if (child.warnings && child.warnings.length) {
-            compilation.errors.push(
-              `Error in child compiler${
-                child.name ? ` ${child.name}` : ' '
-              }: Warnings are not allowed.`
-            );
-            result = false;
-          }
-        });
-      }
-      return result;
+      compilation.children.forEach((child) => {
+        if (child.warnings.length > 0) {
+          child.errors = child.errors.concat(child.warnings);
+          child.warnings = [];
+        }
+      });
     });
   };
 };
